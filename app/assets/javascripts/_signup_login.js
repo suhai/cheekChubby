9ddb7ef6103ba6336d8bf8594ee630e9b82a479b
suhai yehuza <soyehuza@gmail.com>
2017-07-30 03:42:20 -0700

    created homepage buttons

---
+++ app/assets/javascripts/_signup_login.js
@@ -0,0 +1,8 @@
+//  $('.fliper-btn').click(function(){
+//     $('.flip').find('.card').toggleClass('flipped');
+
+// });
+
+
+$('#myModal').modal('show');
+// www.jquery2dotnet.com
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,35 @@
 Rails.application.routes.draw do
-  
+  resources :categories, only: [:show]
+  resources :photo_comments, only: [:show, :create]
+  resources :photos, only: [:show]
+  resources :post_comments, only: [:show, :create]
+  resources :posts, only: [:show]
 
-  get 'categories/show'
+  resources :posts do
+    resources :post_comments
+  end
+
+  resources :photos do
+    resources :photos_comments
+  end
+
+  namespace :admin do
+    resources :categories
+    resources :photo_comments
+    resources :photos
+    resources :post_comments
+    resources :posts
+    resources :sessions
+    resources :users
+  end
+
+  namespace :admin do
+    get "logout" => "sessions#destroy", :as => "logout"
+  end
+
+  namespace :admin do
+    get "login" => "sessions#new", :as => "login"
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#page'
