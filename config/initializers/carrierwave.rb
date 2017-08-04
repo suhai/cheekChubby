3321fa77a3c3bf4736daa29e99410037b637d492
suhai yehuza <soyehuza@gmail.com>
2017-08-04 00:52:18 -0700

    second attempt at configuring aws buckets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                => ENV['ARTIFACTS_REGION']
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['ARTIFACTS_BUCKET']    
+    config.fog_directory     =  ENV['S3_BUCKET_NAME']    
   end
 end
 
